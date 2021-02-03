e1856dd5b8ecc5cbd32e09dd66f2b27a4e653f97
Tsheten Renzee Sherpa <trex33160@gmail.com>
2021-02-03 21:58:44 +0545

    added state value to input, prevNumber, currentNumber, opertator

--- src/App.js
+++ src/App.js
@@ -5,49 +5,57 @@ import {useState} from 'react';
 
 const App = () => {
     
-  const [screen, setScreen] = useState('0')
-  
-  const onClick = (e) => {
-   
-     if(e.target.id === "equals") {
-const result = eval(screen).toString()
+  const [input, setInput] = useState('0')
+  const [prevNumber, setPrevNumber] = useState('')
+  const [currentNumber, setCurrentNumber] = useState('')
+  const [operator, setOperator] = useState('')
 
-setScreen(result)
-     }
-
-     else if(e.target.id === "clear") {
-      setScreen('0')
+  const pressNum = (e) => {
+    if(input === '0') {
+      setInput(e.target.value)
+    } else {
+   setInput(input + e.target.value)
     }
-   
-   
-
-else {   
-  if(screen === "0") {
-  setScreen(e.target.value)
-  } else {
-    setScreen(screen+e.target.value)
   }
+
+  const pressZero = (e) => {
+    if(input !== '0') {
+      setInput(input + e.target.value)
+    }
+  }
+  
+  const pressDecimal = (e) => {
+if(input.indexOf('.') === -1) {
+  setInput(input + e.target.value)
 }
   }
 
-  return (
-    <div id="calculator" className="calculator" onClick={onClick}>
-                
-    <div id="display" className="display">{screen}</div>
-    <div className="nums-container">
-    <button id="clear" className="light-grey big-w">AC</button>
+const clearInput = () => {
+  setInput('0')
+}
 
-    <button id="zero" className='dark-grey' value='0'>0</button>
-    <button id="one" className='dark-grey' value='1'>1</button>
-    <button id="two" className='dark-grey' value='2'>2</button>
-    <button id="three" className='dark-grey' value='3'>3</button>
-    <button id="four" className='dark-grey' value='4'>4</button>
-    <button id="five" className='dark-grey' value='5'>5</button>
-    <button id="six" className='dark-grey' value='6'>6</button>
-    <button id="seven" className='dark-grey' value='7'>7</button>
-    <button id="eight" className='dark-grey' value='8'>8</button>
-    <button id="nine" className='dark-grey' value='9'>9</button>
-    <button id="decimal" className='dark-grey' value='.'>.</button>
+const add = (e) => {
+
+}
+
+  return (
+    <div id="calculator" className="calculator">
+                
+    <div id="display" className="display">{input}</div>
+    <div className="nums-container">
+    <button id="clear" className="light-grey big-w" onClick={clearInput}>AC</button>
+
+    <button id="zero" className='dark-grey' value='0' onClick={pressZero}>0</button>
+    <button id="one" className='dark-grey' value='1' onClick={pressNum}>1</button>
+    <button id="two" className='dark-grey' value='2' onClick={pressNum}>2</button>
+    <button id="three" className='dark-grey' value='3' onClick={pressNum}>3</button>
+    <button id="four" className='dark-grey' value='4' onClick={pressNum}>4</button>
+    <button id="five" className='dark-grey' value='5' onClick={pressNum}>5</button>
+    <button id="six" className='dark-grey' value='6' onClick={pressNum}>6</button>
+    <button id="seven" className='dark-grey' value='7' onClick={pressNum}>7</button>
+    <button id="eight" className='dark-grey' value='8' onClick={pressNum}>8</button>
+    <button id="nine" className='dark-grey' value='9' onClick={pressNum}>9</button>
+    <button id="decimal" className='dark-grey' value='.' onClick={pressDecimal}>.</button>
 
     </div>
     <div className="ops-container">
