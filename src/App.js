f7a9e020677f1133b98cf054d405509b53434be3
tsheten <trex33160@gmail.com>
2021-03-04 19:47:36 +0545

    fixed the styles of calculator

--- src/App.js
+++ src/App.js
@@ -48,26 +48,26 @@ const pressOperator = (e) => {
     
     <div className="nums-container">
     <button id="clear" className="light-grey big-w" onClick={clearInput}>AC</button>
-
-    <button id="zero" className='dark-grey' value='0' onClick={pressZero}>0</button>
-    <button id="one" className='dark-grey' value='1' onClick={pressNum}>1</button>
-    <button id="two" className='dark-grey' value='2' onClick={pressNum}>2</button>
-    <button id="three" className='dark-grey' value='3' onClick={pressNum}>3</button>
-    <button id="four" className='dark-grey' value='4' onClick={pressNum}>4</button>
-    <button id="five" className='dark-grey' value='5' onClick={pressNum}>5</button>
-    <button id="six" className='dark-grey' value='6' onClick={pressNum}>6</button>
+        
     <button id="seven" className='dark-grey' value='7' onClick={pressNum}>7</button>
     <button id="eight" className='dark-grey' value='8' onClick={pressNum}>8</button>
     <button id="nine" className='dark-grey' value='9' onClick={pressNum}>9</button>
+    <button id="four" className='dark-grey' value='4' onClick={pressNum}>4</button>
+    <button id="five" className='dark-grey' value='5' onClick={pressNum}>5</button>
+    <button id="six" className='dark-grey' value='6' onClick={pressNum}>6</button>
+    <button id="one" className='dark-grey' value='1' onClick={pressNum}>1</button>
+    <button id="two" className='dark-grey' value='2' onClick={pressNum}>2</button>
+    <button id="three" className='dark-grey' value='3' onClick={pressNum}>3</button>
+    <button id="zero" className='dark-grey big-w' value='0' onClick={pressZero}>0</button>
     <button id="decimal" className='dark-grey' value='.' onClick={pressDecimal}>.</button>
 
     </div>
     <div className="ops-container">
-    <button id="add" value='+' onClick={pressOperator}>+</button>
-    <button id="subtract" value='-' onClick={pressOperator}>-</button>
-    <button id="multiply" value='*' onClick={pressOperator}>*</button>
     <button id="divide" value='/' onClick={pressOperator}>/</button>
-    <button id="equals" onClick={evaluate}>=</button>
+    <button id="multiply" value='*' onClick={pressOperator}>*</button>
+    <button id="subtract" value='-' onClick={pressOperator}>-</button>
+    <button id="add" value='+' onClick={pressOperator}>+</button>
+    <button id="equals" className="orange" onClick={evaluate}>=</button>
     </div>
   </div>  
   )
